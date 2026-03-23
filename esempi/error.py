7aec7d4e1eb965076ddf55237141bbb943ac4265
Daniel Colla <daniel.colla@delphis.it>
2026-03-23 09:02:27 +0100

    spostamento file per rigorganizzazione

---
+++ esempi/error.py
@@ -0,0 +1,17 @@
+num_a = "10.54a"
+num_b = 3
+
+def calculate_total(price, quantity):
+    return price * quantity
+
+total = calculate_total(num_a, num_b)
+print(f"Your total is: {total}") 
+
+num_a = 0
+print(f"num_a is: {num_a}")
+
+#num_c = "pippo"
+#num_d = 3
+#
+#bad_total = calculate_total(num_c, num_d)
+#print(f"Your total is: {bad_total}") 
